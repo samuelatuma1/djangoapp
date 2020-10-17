d395ff3cca7a0fcfb94ad7eb045e2279ff840173
samuelatuma1 <atumasaake@gmail.com>
2020-10-17 19:31:53 +0100

    made minor changes

--- azuresite/production.py
+++ azuresite/production.py
@@ -1,6 +1,8 @@
 from .settings import *
 import os
 
+
+DEBUG=os.environ['DEBUG']
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
